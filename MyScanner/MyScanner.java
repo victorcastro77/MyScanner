d76afee1d83d4a3ba130ebbeaf5214f228f7119a
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-11-17 11:44:29 -0800

    scanner

--- MyScanner/MyScanner.java
+++ MyScanner/MyScanner.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class MyScanner
 {
-    public void run1() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            if (scanner.hasNext()) {
-                int i = scanner.nextInt();
-                System.out.println(i*3);
-            }
-        }
-    }
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -24,6 +15,5 @@ public class MyScanner
     public static void main(String[] args) {
         MyScanner myScanner = new MyScanner();
         myScanner.run();
-        myScanner.run1();
     }
 }
